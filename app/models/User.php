0ab6217cdc4ef3914adbf0191ce0b7809891a70a
Dmitry Kushneriov <remkwadriga@yandex.ua>
2015-08-03 12:14:13 +0300

    First commit

---
+++ app/models/User.php
@@ -0,0 +1,135 @@
+<?php
+
+use \Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Validator\Email;
+
+class User extends Model
+{
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     *
+     * @var string
+     */
+    public $password_hash;
+
+    /**
+     *
+     * @var string
+     */
+    public $first_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $last_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $phone;
+
+    /**
+     *
+     * @var string
+     */
+    public $reg_date;
+
+    /**
+     *
+     * @var string
+     */
+    public $last_visit_date;
+
+    /**
+     *
+     * @var string
+     */
+    public $role;
+
+    /**
+     *
+     * @var string
+     */
+    public $status;
+
+    /**
+     *
+     * @var string
+     */
+    public $info;
+
+    /**
+     *
+     * @var integer
+     */
+    public $rating;
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation()
+    {
+        $this->validate(
+            new Email(
+                array(
+                    'field'    => 'email',
+                    'required' => true,
+                )
+            )
+        );
+
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'user';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return User[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return User
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
